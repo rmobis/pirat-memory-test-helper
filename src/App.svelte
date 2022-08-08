387e55a0934f2b717fc25a3353996474788140c4
Raphael Mobis Tacla <r.mobis@gmail.com>
2022-08-08 17:52:52 -0300

    fix spacing

--- src/App.svelte
+++ src/App.svelte
@@ -24,7 +24,7 @@
 </script>
 
 <main class="container mx-auto">
-	<div class="flex flex-col gap-y-4 items-center p-4">
+	<div class="flex flex-col gap-y-8 items-center p-4">
 		<div class="bg-cover grid grid-cols-2 gap-x-28 w-[700px] h-[587px]" style:background-image="url({backdrop})">
 			<div class:invisible="{pickedDiagonal !== 2 || pickedCorner !== 2}" class="w-28 h-28 bg-green-500 opacity-50 justify-self-end"></div>
 			<div class:invisible="{pickedDiagonal !== 3 || pickedCorner !== 0}" class="w-28 h-28 bg-green-500 opacity-50"></div>
@@ -47,7 +47,7 @@
 			{/each}
 		</div>
 
-		<div class="flex flex-row gap-x-4 justify-center w-full">
+		<div class="flex flex-row gap-x-8 justify-center w-full">
 			{#each corners as corner, i}
 				<label class="flex flex-col gap-2 p-2 bg-gray-200 rounded border-2 border-gray-400 cursor-pointer" class:bg-gray-400={pickedCorner === i} class:border-gray-700={pickedCorner === i} class:hidden={Math.floor(pickedDiagonal/2) === i}>
 					<input type="radio" name="pickedCorner" bind:group={pickedCorner} value={i}>
