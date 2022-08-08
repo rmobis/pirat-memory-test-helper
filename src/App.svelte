2de5a42be2ab9578a3834d7e79ae2e7406c8e789
Raphael Mobis Tacla <r.mobis@gmail.com>
2022-08-08 20:12:43 -0300

    make it 'responsive'

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,7 @@
 	import Pattern, { images as i } from './lib/Pattern.svelte';
 
 	let pickedDiagonal = 0;
-	let pickedCorner = 1;;
-
-	$: console.log(pickedDiagonal);
+	let pickedCorner = 1;
 
 	const starfishLeftDiag = [i.STARFISH, i.NONE, i.NONE, i.NONE, i.STARFISH, i.NONE, i.NONE, i.NONE, i.STARFISH];
 	const starfishRightDiag = [i.NONE, i.NONE, i.STARFISH, i.NONE, i.STARFISH, i.NONE, i.STARFISH, i.NONE, i.NONE];
@@ -21,17 +19,18 @@
 
 <main class="container mx-auto">
 	<div class="flex flex-col gap-y-8 items-center p-4">
-		<div class="bg-cover grid grid-cols-2 gap-x-28 w-[700px] h-[587px]" style:background-image="url({backdrop})">
-			<div class:invisible="{pickedDiagonal !== 2 || pickedCorner !== 2}" class="w-28 h-28 bg-green-500 opacity-50 justify-self-end"></div>
-			<div class:invisible="{pickedDiagonal !== 3 || pickedCorner !== 0}" class="w-28 h-28 bg-green-500 opacity-50"></div>
-			<div class:invisible="{pickedDiagonal !== 1 || pickedCorner !== 1}" class="w-28 h-28 bg-green-500 opacity-50"></div>
-			<div class:invisible="{pickedDiagonal !== 1 || pickedCorner !== 2}" class="w-28 h-28 bg-green-500 opacity-50 justify-self-end"></div>
-			<div class="h-24 col-span-2 opacity-0"></div>
-			<div class:invisible="{pickedDiagonal !== 2 || pickedCorner !== 0}" class="w-28 h-28 bg-green-500 opacity-50"></div>
-			<div class:invisible="{pickedDiagonal !== 0 || pickedCorner !== 2}" class="w-28 h-28 bg-green-500 opacity-50 justify-self-end"></div>
-			<div class="h-6 col-span-2 opacity-0"></div>
-			<div class:invisible="{pickedDiagonal !== 0 || pickedCorner !== 1}" class="w-28 h-28 bg-green-500 opacity-50 justify-self-end"></div>
-			<div class:invisible="{pickedDiagonal !== 3 || pickedCorner !== 2}" class="w-28 h-28 bg-green-500 opacity-50"></div>
+		<div class="relative max-w-[700px] max-h-[587px]">
+			<img src={backdrop} alt="" class=" w-full">
+			<div class="backdrop absolute top-0 left-0 grid gap-y-[1px] w-full h-full">
+				<div class:invisible="{pickedDiagonal !== 2 || pickedCorner !== 2}" class="marker even:col-start-3 justify-self-end"></div>
+				<div class:invisible="{pickedDiagonal !== 3 || pickedCorner !== 0}" class="marker even:col-start-3"></div>
+				<div class:invisible="{pickedDiagonal !== 1 || pickedCorner !== 1}" class="marker even:col-start-3"></div>
+				<div class:invisible="{pickedDiagonal !== 1 || pickedCorner !== 2}" class="marker even:col-start-3 justify-self-end"></div>
+				<div class:invisible="{pickedDiagonal !== 2 || pickedCorner !== 0}" class="marker even:col-start-3 row-start-4"></div>
+				<div class:invisible="{pickedDiagonal !== 0 || pickedCorner !== 2}" class="marker even:col-start-3 row-start-4 justify-self-end"></div>
+				<div class:invisible="{pickedDiagonal !== 0 || pickedCorner !== 1}" class="marker even:col-start-3 row-start-6 justify-self-end"></div>
+				<div class:invisible="{pickedDiagonal !== 3 || pickedCorner !== 2}" class="marker even:col-start-3 row-start-6"></div>
+			</div>
 		</div>
 
 		<div class="flex flex-row gap-x-8 justify-center w-full">
@@ -56,4 +55,17 @@
 </main>
 
 <style>
+	.backdrop {
+		grid-template:
+			"a . b" 1fr
+			"c . d" 1fr
+			". . ." 0.8fr
+			"e . f" 1fr
+			". . ." 0.3fr
+			"g . h" 1fr / 2.75fr 1fr 2.75fr;
+	}
+	.marker {
+		@apply w-[38%] h-full bg-green-500 opacity-50;
+	}
+
 </style>
